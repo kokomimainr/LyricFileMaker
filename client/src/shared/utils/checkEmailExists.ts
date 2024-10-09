1e0127864dce2e96f7cf8aeb5c52d4637da1bec2
Anna Shifrina <shifrina19anna@gmail.com>
2024-10-09 19:57:43 +0300

    feat: Fixed check email

--- client/src/shared/utils/checkEmailExists.ts
+++ client/src/shared/utils/checkEmailExists.ts
@@ -3,7 +3,7 @@ import { axiosInstance } from '../lib/axiosInstance';
 export const checkEmailExists = async (email: string): Promise<boolean> => {
   try {
     const response = await axiosInstance.post('/auth/check-email', { email });
-    return response.data.exists === true;
+    return response.data.exists;
   } catch (error) {
     console.error('Error checking email existence:', error);
     return false;
--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -1,26 +1,38 @@
 const cookiesConfig = require("../configs/cookiesConfig");
 const userService = require("../services/userService");
 const generateTokens = require("../utils/generateToken");
-const emailExistence = require("email-existence");
-const upload = require("../utils/upload");
+const axios = require('axios');
 
 async function checkEmailExistence(req, res) {
   const { email } = req.body;
   if (!email) {
     return res
       .status(400)
-      .json({ exists: false, message: "Email is required" });
+      .json({ exists: false, message: 'Email is required' });
   }
 
-  emailExistence.check(email, (error, result) => {
-    if (error) {
-      console.error(error);
-      return res
-        .status(500)
-        .json({ exists: false, message: "Internal Server Error" });
+  try {
+    const apiKey = 'e8ced4041df8fa076999afb5f93bb01c73715781'; //* этот apiKey
+    const response = await axios.get(
+      `https://api.hunter.io/v2/email-verifier?email=${email}&api_key=${apiKey}`
+    );
+
+    const { data } = response;
+
+    if (data.data && data.data.result === 'undeliverable') {
+      return res.status(200).json({ exists: false });
     }
-    return res.status(200).json({ exists: result });
-  });
+
+    return res.status(200).json({
+      exists: data.data.result === 'deliverable',
+      message: data.data.status,
+    });
+  } catch (error) {
+    console.error('Error checking email:', error);
+    return res
+      .status(500)
+      .json({ exists: false, message: 'Internal Server Error' });
+  }
 }
 
 async function signUp(req, res) {
